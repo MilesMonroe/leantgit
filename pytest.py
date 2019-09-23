3fff147807b91d1d37ef4a5d98529fed5d982c85
MilesMonroe <hdmsfhfg@qq.com>
2019-09-23 14:34:42 +0800

    modify1 python

--- pytest.py
+++ pytest.py
@@ -3,9 +3,14 @@ print("pls input any chars")
 name=input()
 print("you input")
 print(name)
-a=100
+#a=100
+a=1000
 if a>10:
 	print(a)
 else:
 	print(-a)
+	
+print("git1 modify test")
+
+print("git2 modify test")
 	
